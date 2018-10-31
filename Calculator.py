153ca426e3e0a5333a42105795f23a2e7ef90fa7
Murropoly <42419333+Murropoly@users.noreply.github.com>
2018-10-30 19:35:44 -0700

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,12 +1,12 @@
 """
 *Project: THE PROGRAMIMG CALCULATOR
 *Programmer: Tyler Muranaka
-*Version: 1.5
+*Version: 2.0
 """
 
 #Menu screen
 print("""
-Welcome to The Programming Calculator!!! \n
+Welcome to The Programming Calculator!!!\n
 Type one:
 *Addition()
 *Subtraction()
@@ -17,30 +17,30 @@ Type one:
 
 #Addition
 def Addition():
-    first = int(input('What is your first number'))
-    second = int(input('What is your second number.'))
+    first = int(input("Type First Number: "))
+    second = int(input("Type Second Number: "))
     print(first + second)
     
 #Subtraction
 def Subtraction():
-    first = int(input('What is your first number?'))
-    second = int(input('What is your second number?'))
+    first = int(input("Type First Number: "))
+    second = int(input("Type Second Number: "))
     print(first - second)
  
 #Multiplication 
 def Multiplication():
-    first = int(input('What is your first number?'))
-    second = int(input('What is your second number?'))
+    first = int(input("Type First Number: "))
+    second = int(input("Type Second Number: "))
     print(first * second)
     
 #Division
 def Division():
-    first = int(input('What is your first number?'))
-    second = int(input('What is your second number?'))
+    first = int(input("Type First Number: "))
+    second = int(input("Type Second Number: "))
     print(first / second)
     
 #Modulo
 def Modulo():
-    first = int(input('What is your first number?'))
-    second = int(input('What is your second number?'))
+    first = int(input("Type First Number: "))
+    second = int(input("Type Second Number: "))
     print(first % second)
